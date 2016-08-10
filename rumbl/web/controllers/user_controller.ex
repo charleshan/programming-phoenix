10596655d7b547b2f15fc1054f6cb720e077d19d
Charles Changmin Han <name.cmh@gmail.com>
2016-08-09 22:08:58 -0700

    Add show function to UserController.

--- rumbl/web/controllers/user_controller.ex
+++ rumbl/web/controllers/user_controller.ex
@@ -5,4 +5,9 @@ defmodule Rumbl.UserController do
     users = Repo.all(Rumbl.User)
     render conn, "index.html", users: users
   end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(Rumbl.User, id)
+    render conn, "show.html", user: user
+  end
 end
