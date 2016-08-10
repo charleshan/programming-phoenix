dca329d91c9864a0841993b8e2827deb3dadf7b7
Charles Changmin Han <name.cmh@gmail.com>
2016-08-09 23:52:47 -0700

    Add create function to UserController.

--- rumbl/web/controllers/user_controller.ex
+++ rumbl/web/controllers/user_controller.ex
@@ -17,4 +17,13 @@ defmodule Rumbl.UserController do
     changeset = User.changeset(%User{})
     render conn, "new.html", changeset: changeset
   end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    {:ok, user} = Repo.insert(changeset)
+
+    conn
+    |> put_flash(:info, "#{user.name} created!")
+    |> redirect(to: user_path(conn, :index))
+  end
 end
