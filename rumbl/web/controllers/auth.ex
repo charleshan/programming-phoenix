cf01072d40d81b19dedb5ae51008421d3e62dc7f
Charles Changmin Han <name.cmh@gmail.com>
2016-08-10 01:28:57 -0700

    Add auth controller.

---
+++ rumbl/web/controllers/auth.ex
@@ -0,0 +1,13 @@
+defmodule Rumbl.Auth do
+  import Plug.Conn
+
+  def init(opts) do
+    Keyword.fetch!(opts, :repo)
+  end
+
+  def call(conn, repo) do
+    user_id = get_session(conn, :user_id)
+    user    = user_id && repo.get(Rumbl.User, user_id)
+    assign(conn, :current_user, user)
+  end
+end
