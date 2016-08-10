9d14363dc938cb7b10e06a09f6f75f716016415b
Charles Changmin Han <name.cmh@gmail.com>
2016-08-09 21:18:53 -0700

    Add fuctions to get user by id or attribute.

--- rumbl/lib/rumbl/repo.ex
+++ rumbl/lib/rumbl/repo.ex
@@ -9,4 +9,14 @@ defmodule Rumbl.Repo do
      %Rumbl.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}]
   end
   def all(_module), do: []
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val
+      end)
+    end
+  end
 end
