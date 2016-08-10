7548ce0a86c9f2034a709ad5355b64504ef1c550
Charles Changmin Han <name.cmh@gmail.com>
2016-08-09 23:57:30 -0700

    Handle error raised on insertion.

--- rumbl/web/controllers/user_controller.ex
+++ rumbl/web/controllers/user_controller.ex
@@ -20,10 +20,13 @@ defmodule Rumbl.UserController do
 
   def create(conn, %{"user" => user_params}) do
     changeset = User.changeset(%User{}, user_params)
-    {:ok, user} = Repo.insert(changeset)
-
-    conn
-    |> put_flash(:info, "#{user.name} created!")
-    |> redirect(to: user_path(conn, :index))
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
