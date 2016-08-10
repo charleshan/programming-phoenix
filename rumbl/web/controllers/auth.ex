1da8a0ac48dde775a8ce1027ef4ce1c0a0bab03c
Charles Changmin Han <name.cmh@gmail.com>
2016-08-10 01:56:42 -0700

    Add login function.

--- rumbl/web/controllers/auth.ex
+++ rumbl/web/controllers/auth.ex
@@ -10,4 +10,11 @@ defmodule Rumbl.Auth do
     user    = user_id && repo.get(Rumbl.User, user_id)
     assign(conn, :current_user, user)
   end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
 end
