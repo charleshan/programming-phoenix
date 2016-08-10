efa4d0bc467d235e08827c001035d94d17a1b4c3
Charles Changmin Han <name.cmh@gmail.com>
2016-08-09 21:09:28 -0700

    Add in memory temporary repository.

--- rumbl/lib/rumbl/repo.ex
+++ rumbl/lib/rumbl/repo.ex
@@ -1,3 +1,12 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+  #use Ecto.Repo, otp_app: :rumbl
+  @moduledoc """
+  In memory repository.
+  """
+  def all(Rumbl.User) do
+    [%Rumbl.User{id: "1", name: "Jose", username: "josevalim", password: "elixir"},
+     %Rumbl.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
+     %Rumbl.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}]
+  end
+  def all(_module), do: []
 end
