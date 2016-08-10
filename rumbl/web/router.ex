5b1b5effc9499fea47e9de349d6e59c8c0f95a4a
Charles Changmin Han <name.cmh@gmail.com>
2016-08-09 23:38:21 -0700

    Replace current routes to keyword resources.

--- rumbl/web/router.ex
+++ rumbl/web/router.ex
@@ -16,9 +16,8 @@ defmodule Rumbl.Router do
   scope "/", Rumbl do
     pipe_through :browser # Use the default browser stack
 
-    get "/users", UserController, :index
-    get "/users/:id", UserController, :show
     get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.
