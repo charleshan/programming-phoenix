c37916774c3e8623989bd48d4240265b5255ed2a
Charles Changmin Han <name.cmh@gmail.com>
2016-08-10 02:18:13 -0700

    Add function to login.

--- rumbl/web/controllers/session_controller.ex
+++ rumbl/web/controllers/session_controller.ex
@@ -4,4 +4,17 @@ defmodule Rumbl.SessionController do
   def new(conn, _) do
     render conn, "new.html"
   end
+
+  def create(conn, %{"session" => %{"username" => user, "password" => pass}}) do
+    case Rumbl.Auth.login_by_username_and_pass(conn, user, pass, repo: Repo) do
+      {:ok, conn} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> redirect(to: page_path(conn, :index))
+      {:error, _reason, conn} ->
+        conn
+        |> put_flash(:error, "Invalid username/password combination")
+        |> render("new.html")
+    end
+  end
 end
