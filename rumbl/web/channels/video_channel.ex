b14fa6b5253f1a4f246977d7c7f74cfb7e388bbe
Charles Changmin Han <name.cmh@gmail.com>
2016-08-11 15:16:16 -0700

    Add backend to handle new_annotnation.

--- rumbl/web/channels/video_channel.ex
+++ rumbl/web/channels/video_channel.ex
@@ -2,14 +2,16 @@ defmodule Rumbl.VideoChannel do
   use Rumbl.Web, :channel
 
   def join("videos:" <> video_id, _params, socket) do
-    :timer.send_interval(5_000, :ping)
     {:ok, socket}
   end
 
-  def handle_info(:ping, socket) do
-    count = socket.assigns[:count] || 1
-    push socket, "ping", %{count: count}
-    
-    {:noreply, assign(socket, :count, count + 1)}
+  def handle_in("new_annotation", params, socket) do
+    broadcast! socket, "new_annotation", %{
+      user: %{username: "anno"},
+      body: params["body"],
+      at: params["at"]
+    }
+
+    {:reply, :ok, socket}
   end
 end
