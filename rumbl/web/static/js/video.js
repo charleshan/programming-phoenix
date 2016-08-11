977beeb67f9d15d4c2903e400c61046197541d34
Charles Changmin Han <name.cmh@gmail.com>
2016-08-11 12:05:28 -0700

    Join channel.

--- rumbl/web/static/js/video.js
+++ rumbl/web/static/js/video.js
@@ -17,7 +17,10 @@ let Video = {
     let msgInput = document.getElementById("msg-input")
     let postButton = document.getElementById("msg-submit")
     let vidChannel = socket.channel("videos:" + videoId)
-    // TODO join the vidChannel
+    
+    vidChannel.join()
+      .receive("ok", resp => console.log("joined the video channel", resp))
+      .receive("error", reason => console.log("join failed", reason))
   }
 }
 export default Video
