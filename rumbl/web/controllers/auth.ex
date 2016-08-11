11d126bf8a927f8832005d9536f0fa80c6a9d838
Charles Changmin Han <name.cmh@gmail.com>
2016-08-11 15:40:39 -0700

    Assign user_token.

--- rumbl/web/controllers/auth.ex
+++ rumbl/web/controllers/auth.ex
@@ -10,18 +10,26 @@ defmodule Rumbl.Auth do
     user_id = get_session(conn, :user_id)
     
     cond do
-      user = conn.assigns[:current_user] -> conn
+      user = conn.assigns[:current_user] -> put_current_user(conn, user) 
       user = user_id && repo.get(Rumbl.User, user_id) ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
       true -> assign(conn, :current_user, nil)
     end
   end
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
   def login_by_username_and_pass(conn, username, given_pass, opts) do
