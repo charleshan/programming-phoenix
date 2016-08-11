2858017e07ddf585df24feff8af3f03eee0f3fe6
Charles Changmin Han <name.cmh@gmail.com>
2016-08-10 18:06:15 -0700

    Add test for unauthorized user.

--- rumbl/test/controllers/video_controller_test.exs
+++ rumbl/test/controllers/video_controller_test.exs
@@ -57,4 +57,24 @@ defmodule Rumbl.VideoControllerTest do
     assert html_response(conn, 200) =~ "check the errors"
     assert video_count(Video) == count_before
   end
+
+  @tag login_as: "max"
+  test "authorizes actions against access by other users", %{user: owner, conn: conn} do
+    video = insert_video(owner, @valid_attrs)
+    non_owner = insert_user(username: "sneaky")
+    conn = assign(conn, :current_user, non_owner)
+
+    assert_error_sent :not_found, fn ->
+      get(conn, video_path(conn, :show, video))
+    end
+    assert_error_sent :not_found, fn ->
+      get(conn, video_path(conn, :edit, video))
+    end
+    assert_error_sent :not_found, fn ->
+      put(conn, video_path(conn, :update, video, video: @valid_attrs))
+    end
+    assert_error_sent :not_found, fn ->
+      delete(conn, video_path(conn, :delete, video))
+    end
+  end
 end
