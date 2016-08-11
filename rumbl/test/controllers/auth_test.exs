dcb6877ca96c0a63dc6602c604d24501436d34c6
Charles Changmin Han <name.cmh@gmail.com>
2016-08-10 19:04:38 -0700

    Add tests for Auth.call

--- rumbl/test/controllers/auth_test.exs
+++ rumbl/test/controllers/auth_test.exs
@@ -43,4 +43,18 @@ defmodule Rumbl.AuthTest do
     next_conn = get(logout_conn, "/")
     refute get_session(next_conn, :user_id)
   end
+
+  test "call places user from session into assigns", %{conn: conn} do
+    user = insert_user()
+    conn =
+      conn
+      |> put_session(:user_id, user.id)
+      |> Auth.call(Repo)
+    assert conn.assigns.current_user.id == user.id
+  end
+
+  test "call with no session sets current_user assign to nil", %{conn: conn} do
+    conn = Auth.call(conn, Repo)
+    assert conn.assigns.current_user == nil
+  end
 end
