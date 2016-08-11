96c3bf01d6cd5a68a15eacf0c464bee8b7fec6c6
Charles Changmin Han <name.cmh@gmail.com>
2016-08-11 15:02:20 -0700

    Implement pushing and receiving new_annotation.

--- rumbl/web/static/js/video.js
+++ rumbl/web/static/js/video.js
@@ -18,9 +18,25 @@ let Video = {
     let postButton = document.getElementById("msg-submit")
     let vidChannel = socket.channel("videos:" + videoId)
     
+    postButton.addEventListener("click", e => {
+      let payload = {body: msgInput.value, at: Player.getCurrentTime()}
+      vidChannel.push("new_annotation", payload)
+                .receive("error", e => console.log(e) )
+      msgInput.value = ""
+    })
+
+    vidChannel.on("new_annotation", (resp) => {
+      this.renderAnnotation(msgContainer, resp)
+    })
+
     vidChannel.join()
       .receive("ok", resp => console.log("joined the video channel", resp))
       .receive("error", reason => console.log("join failed", reason))
+  },
+
+  renderAnnotation(msgContainer, {user, body, at}) {
+    // TODO append annotation to msgContainer
+  
   }
 }
 export default Video
