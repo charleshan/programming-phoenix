2b57684c6722effef770d640e45174e40d16558b
Charles Changmin Han <name.cmh@gmail.com>
2016-08-10 17:18:31 -0700

    Add test to test logged in users.

--- rumbl/test/controllers/video_controller_test.exs
+++ rumbl/test/controllers/video_controller_test.exs
@@ -15,4 +15,20 @@ defmodule Rumbl.VideoControllerTest do
       assert conn.halted
     end)
   end
+
+  setup do
+    user = insert_user(username: "max")
+    conn = assign(conn(), :current_user, user)
+    {:ok, conn: conn, user: user}
+  end
+
+  test "list all user's videos on index", %{conn: conn, user: user} do
+    user_video = insert_video(user, title: "funny cats")
+    other_video = insert_video(insert_user(username: "other"), title: "another video")
+
+    conn = get conn, video_path(conn, :index)
+    assert html_response(conn, 200) =~ ~r/Listing videos/
+    assert String.contains?(conn.resp_body, user_video.title)
+    refute String.contains?(conn.resp_body, other_video.title)
+  end
 end
