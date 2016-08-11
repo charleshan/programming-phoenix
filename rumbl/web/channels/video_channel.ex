96c4c396f688757a4dca4f643f1f4fc6e9a87f86
Charles Changmin Han <name.cmh@gmail.com>
2016-08-11 14:49:12 -0700

    Add ping counter to socket.

--- rumbl/web/channels/video_channel.ex
+++ rumbl/web/channels/video_channel.ex
@@ -2,6 +2,14 @@ defmodule Rumbl.VideoChannel do
   use Rumbl.Web, :channel
 
   def join("videos:" <> video_id, _params, socket) do
-    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+    :timer.send_interval(5_000, :ping)
+    {:ok, socket}
+  end
+
+  def handle_info(:ping, socket) do
+    count = socket.assigns[:count] || 1
+    push socket, "ping", %{count: count}
+    
+    {:noreply, assign(socket, :count, count + 1)}
   end
 end
