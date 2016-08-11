a9861ce6b3c81749b567497bd33e3faec8b70b9b
Charles Changmin Han <name.cmh@gmail.com>
2016-08-11 16:24:25 -0700

    Persist annotations.

--- rumbl/web/channels/video_channel.ex
+++ rumbl/web/channels/video_channel.ex
@@ -2,16 +2,31 @@ defmodule Rumbl.VideoChannel do
   use Rumbl.Web, :channel
 
   def join("videos:" <> video_id, _params, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
   end
 
-  def handle_in("new_annotation", params, socket) do
-    broadcast! socket, "new_annotation", %{
-      user: %{username: "anno"},
-      body: params["body"],
-      at: params["at"]
-    }
+  def handle_in(event, params, socket) do
+    user = Repo.get(Rumbl.User, socket.assigns.user_id)
+    handle_in(event, params, user, socket)
+  end
 
-    {:reply, :ok, socket}
+  def handle_in("new_annotation", params, user, socket) do
+    changeset =
+      user
+      |> build_assoc(:annotations, video_id: socket.assigns.video_id)
+      |> Rumbl.Annotation.changeset(params)
+
+    case Repo.insert(changeset) do
+      {:ok, annotation} ->
+        broadcast! socket, "new_annotation", %{
+          id: annotation.id,
+          user: Rumbl.UserView.render("user.json", %{user: user}),
+          body: annotation.body,
+          at: annotation.at
+        }
+        {:reply, :ok, socket}
+      {:error, changeset} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 end
