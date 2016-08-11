4a201a0d096fb8e5371390d11eab586052151737
Charles Changmin Han <name.cmh@gmail.com>
2016-08-10 23:10:53 -0700

    Add tests for registration_changeset.

--- rumbl/test/models/user_test.exs
+++ rumbl/test/models/user_test.exs
@@ -19,4 +19,21 @@ defmodule Rumbl.UserTest do
     attrs = Map.put(@valid_attrs, :username, String.duplicate("a", 30))
     assert {:username, "should be at most 20 character(s)"} in errors_on(%User{}, attrs)
   end
+
+  test "registration_changeset password must be at least 6 chars long" do
+    attrs = Map.put(@valid_attrs, :password, "12345")
+    changeset = User.registration_changeset(%User{}, attrs)
+    assert {:password, {"should be at least %{count} character(s)", count: 6}} in changeset.errors
+  end
+
+  test "registration_changeset with valid attributes hashes password" do
+    attrs = Map.put(@valid_attrs, :password, "123456")
+    changeset = User.registration_changeset(%User{}, attrs)
+    
+    %{password: pass, password_hash: pass_hash} = changeset.changes
+
+    assert changeset.valid?
+    assert pass_hash
+    assert Comeonin.Bcrypt.checkpw(pass, pass_hash)
+  end
 end
