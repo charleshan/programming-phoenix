943cebeaf025479394f888da2a1a8f0b897cb570
Charles Changmin Han <name.cmh@gmail.com>
2016-08-10 17:46:56 -0700

    Test insert video.

--- rumbl/test/controllers/video_controller_test.exs
+++ rumbl/test/controllers/video_controller_test.exs
@@ -4,7 +4,7 @@ defmodule Rumbl.VideoControllerTest do
   setup %{conn: conn} = config do
     if username = config[:login_as] do
       user = insert_user(username: username) 
-      conn = assign(conn(), :current_user, user)
+      conn = assign(build_conn(), :current_user, user)
       {:ok, conn: conn, user: user}
     else
       :ok
@@ -35,5 +35,26 @@ defmodule Rumbl.VideoControllerTest do
     assert html_response(conn, 200) =~ ~r/Listing videos/
     assert String.contains?(conn.resp_body, user_video.title)
     refute String.contains?(conn.resp_body, other_video.title)
+  end
+
+  alias Rumbl.Video
+  @valid_attrs %{url: "http://youtu.be", title: "vid", description: "a vid"}
+  @invalid_attrs %{title: "invalid"}
+
+  defp video_count(query), do: Repo.one(from v in query, select: count(v.id))
+
+  @tag login_as: "max"
+  test "creates user video and redirects", %{conn: conn, user: user} do
+    conn = post conn, video_path(conn, :create), video: @valid_attrs
+    assert redirected_to(conn) == video_path(conn, :index)
+    assert Repo.get_by!(Video, @valid_attrs).user_id == user.id
+  end
+
+  @tag login_as: "max"
+  test "does not create video and renders errors when invalid", %{conn: conn} do
+    count_before = video_count(Video)
+    conn = post conn, video_path(conn, :create), video: @invalid_attrs
+    assert html_response(conn, 200) =~ "check the errors"
+    assert video_count(Video) == count_before
   end
 end
