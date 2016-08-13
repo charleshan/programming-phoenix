4c89bdd366b338ca24f61fd7cb0cfa1dc775bc54
Charles Changmin Han <name.cmh@gmail.com>
2016-08-13 14:00:39 -0700

    Add comput/2 test with timeout.

--- rumbrella/apps/info_sys/test/info_sys_test.exs
+++ rumbrella/apps/info_sys/test/info_sys_test.exs
@@ -12,6 +12,21 @@ defmodule InfoSysTest do
     def fetch("none", ref, owner, _limit) do
       send(owner, {:results, ref, []})
     end
+    def fetch("timeout", _ref, owner, _limit) do
+      send(owner, {:backend, self()})
+      :timer.sleep(:infinity)
+    end
+  end
+
+  test "compute/2 with timeout returns no results and kills workers" do
+    results = InfoSys.compute("timeout", backends: [TestBackend], timeout: 10)
+    assert results == []
+    assert_receive {:backend, backend_pid}
+
+    ref = Process.monitor(backend_pid)
+    assert_receive {:DOWN, ^ref, :process, _pid, _reason}
+    refute_received {:DOWN, _, _, _, _}
+    refute_received :timeout
   end
 
   test "compute/2 with backend results" do
