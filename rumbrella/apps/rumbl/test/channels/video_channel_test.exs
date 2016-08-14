bdd9e1565a639f72ec6b30ee231224327bf79bc1
Charles Changmin Han <name.cmh@gmail.com>
2016-08-13 17:15:48 -0700

    Add test for inserting new annotations.

--- rumbrella/apps/rumbl/test/channels/video_channel_test.exs
+++ rumbrella/apps/rumbl/test/channels/video_channel_test.exs
@@ -21,4 +21,11 @@ defmodule Rumbl.Channels.VideoChannelTest do
     assert socket.assigns.video_id == vid.id
     assert %{annotations: [%{body: "one"}, %{body: "two"}]} = reply
   end
+
+  test "inserting new annotations", %{socket: socket, video: vid} do
+    {:ok, _, socket} = subscribe_and_join(socket, "videos:#{vid.id}", %{})
+    ref = push socket, "new_annotation", %{body: "the body", at: 0}
+    assert_reply ref, :ok, %{}
+    assert_broadcast "new_annotation", %{}
+  end
 end
